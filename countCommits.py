991acf3b696a03f8b58012c03b967a6f295d2248
SeppPenner <HansM2013@gmx.de>
2019-09-29 17:00:33 +0200

    Updated python version.

--- countCommits.py
+++ countCommits.py
@@ -1,7 +1,7 @@
 import os
 
 #Script to show the number of git commits in a repository
-#Works with Python Version 3.7.3
+#Works with Python Version 3.7.4
 def countCommits():
 	"countCommits"
 	os.system('git rev-list --all --count')
